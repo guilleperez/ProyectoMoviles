d28fbd37937b2ac34ce47413bad1e5fa60049b18
Guillermo Pérez Trueba <a01377162@imac-de-administrador-101.local>
2018-04-25 18:47:41 -0500

    Initial Commit

---
+++ ProyectoMoviles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoMoviles
+//
+//  Created by Guillermo Pérez Trueba on 25/04/18.
+//  Copyright © 2018 Guillermo Pérez Trueba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
