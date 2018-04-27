301a435f570eaf1c708e3739c1b62a0769077b4e
Guillermo Pérez <a01377162@imac-de-administrador-101.local>
2018-04-27 12:59:00 -0500

    Pantalla Preferencias

--- ProyectoMoviles/SugeridosViewController.swift
+++ ProyectoMoviles/SugeridosViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SugeridosViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource { 
 
+    let colors = ["negro","blanco","gris","amarillo","naranja","rojo","morado", "azul","verde"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,15 +26,42 @@ class SugeridosViewController: UIViewController, UICollectionViewDelegate, UICol
 
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return colors.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "celda", for: indexPath) as! CeldaCollectionViewCell
         
-        celda.backgroundColor = UIColor.green
+        switch indexPath.row{
+        case 0:
+            celda.backgroundColor = UIColor.black
+        case 1:
+            celda.backgroundColor = UIColor.gray
+        case 2:
+            celda.backgroundColor = UIColor.white
+        case 3:
+            celda.backgroundColor = UIColor.yellow
+        case 4:
+            celda.backgroundColor = UIColor.orange
+        case 5:
+            celda.backgroundColor = UIColor.red
+        case 6:
+            celda.backgroundColor = UIColor.purple
+        case 7:
+            celda.backgroundColor = UIColor.blue
+        default:
+            celda.backgroundColor = UIColor.green
+        }
         
-        celda.lblText.text = "hola"
+
+        celda.layer.borderWidth = 1.0
+        celda.layer.borderColor = UIColor.black.cgColor
+        
+        /*celda.backgroundColor = UIColor.white
+        celda.backgroundColor = UIColor.gray
+        celda.backgroundColor = UIColor.green
+        celda.backgroundColor = UIColor.green*/
+        
 
         return celda
     }
