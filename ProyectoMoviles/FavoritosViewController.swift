d94c2f2ff7838f8f55436d80189ca35c742dd291
Rubén Escalante Chan <a01370880@imac-de-administrador-102.local>
2018-04-27 14:00:07 -0500

    Imagenes

---
+++ ProyectoMoviles/FavoritosViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FavoritosViewController.swift
+//  ProyectoMoviles
+//
+//  Created by Rubén Escalante Chan on 27/04/18.
+//  Copyright © 2018 Guillermo Pérez Trueba. All rights reserved.
+//
+
+import UIKit
+
+class FavoritosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tabla: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+        celda.textLabel?.text = "Outfit"
+        
+        return celda
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
