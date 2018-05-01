d5800260e9722265e8f6b70653703073a6f5a64a
irv0304 <A01371240@itesm.mx>
2018-05-01 10:24:52 -0500

    Menu del closet y funcionalidad para ver el closet por cada tipo de ropa.

---
+++ ProyectoMoviles/ViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ViewCell.swift
+//  ProyectoMoviles
+//
+//  Created by Ivan Rangel Varela on 30/04/18.
+//  Copyright © 2018 Guillermo Pérez Trueba. All rights reserved.
+//
+
+import UIKit
+
+class ViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
